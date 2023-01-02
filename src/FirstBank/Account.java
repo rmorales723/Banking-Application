8ba1f754945a2cd56dcca660409b2af8f6a12185
robert morales <rmoralfit@gmail.com>
2023-01-01 20:21:47 -0500

    created deposit function in Account

--- src/FirstBank/Account.java
+++ src/FirstBank/Account.java
@@ -5,8 +5,20 @@ public class Account {
     //Class variables
 	int balance;
 	int prevTransaction;
-	String customerNme;
+	String customerName;
 	String customerId;
 	
 	//Class constructor
+    Account(String cname, String cid){
+		customerName = cname;
+		customerId = cid;
+	}
+	
+	//Function for depositing money
+	void deposit(int amount) {
+		if (amount != 0) {
+			balance = balance + amount;
+			prevTransaction = amount;
+		}
+	}
 }
