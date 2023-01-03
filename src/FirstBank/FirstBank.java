6ee99b0784a94bad55da38f1bc7cfdac5fc50c01
robert morales <rmoralfit@gmail.com>
2023-01-02 19:43:37 -0500

    called showMenu in FirstBank Class

--- src/FirstBank/FirstBank.java
+++ src/FirstBank/FirstBank.java
@@ -4,6 +4,8 @@ public class FirstBank {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Account rob = new Account("Robert Morales", "A763823");
+		rob.showMenu();
 
 	}
 
